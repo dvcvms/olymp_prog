e5a7e544e16b1c4af9c14e4950f385243dcb15d4
max-vivo <dvcvms@gmail.com>
2023-02-01 06:15:10 +0300

    REFACTOR code (set OK status)

--- src/olymp/yandex/training/two/hm3/b/Solution.java
+++ src/olymp/yandex/training/two/hm3/b/Solution.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+//OK
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
