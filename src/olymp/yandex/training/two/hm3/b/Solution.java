0e8869811accad6dfa20832a4cba8c89f4e918b5
max-vivo <dvcvms@gmail.com>
2023-02-01 04:10:47 +0300

    ADD solution hm3 task b

---
+++ src/olymp/yandex/training/two/hm3/b/Solution.java
@@ -0,0 +1,18 @@
+package olymp.yandex.training.two.hm3.b;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
+
+        while (scanner.hasNext()) {
+            int value = scanner.nextInt();
+            System.out.println(set.contains(value) ? "YES" : "NO");
+            set.add(value);
+        }
+    }
+}
